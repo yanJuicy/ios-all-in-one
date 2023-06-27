e23063177e64d732c607d82d08fcc7971717a9c9
yanJuicy <hanjub3@gmail.com>
2023-06-27 22:04:39 +0900

    Alert 띄워보기

--- MyAlbum/MyAlbum/ViewController.swift
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func hello(_ sender: Any) {
+        let alert = UIAlertController(title: "Hello", message: "My First App!", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
