d7ae05179a2f76dc5e66d77a7a743bd32d5c49bc
yanJuicy <hanjub3@gmail.com>
2023-06-29 11:42:58 +0900

    메소드 작성

--- MyAlbum/MyAlbum/ViewController.swift
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -14,19 +14,23 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        priceLabel.text = "₩ \(currentValue)"
+        refresh()
     }
 
     @IBAction func showAlert(_ sender: Any) {
-        let randomPrice = arc4random_uniform(10000) + 1
-        currentValue = Int(randomPrice)
-        priceLabel.text = "₩ \(currentValue)"
         let message = "가격은 \(currentValue) 입니다"
         
         let alert = UIAlertController(title: "Hello", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        refresh()
+    }
+    
+    func refresh() {
+        let randomPrice = arc4random_uniform(10000) + 1
+        currentValue = Int(randomPrice)
+        priceLabel.text = "₩ \(currentValue)"
     }
     
 }
