375ec8cb9a738a6b67c3d9d28e20d6e0bf7f50e6
yanJuicy <hanjub3@gmail.com>
2023-07-19 02:52:52 +0900

    코드 회고

--- BountyList/BountyList/BountyViewController.swift
+++ BountyList/BountyList/BountyViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     let nameList = ["brook", "chopper", "franky", "luffy", "nami", "robin", "sanji", "zoro"]
-    let bountyList = [33000000, 50, 44000000, 3000000000, 80000000, 77000000, 1200000000]
+    let bountyList = [33000000, 50, 44000000, 3000000000, 16000000, 80000000, 77000000, 1200000000]
     
     override func viewDidLoad() {
         super.viewDidLoad()
